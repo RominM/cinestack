577b9b17e83dc5e60013a4f0f8c8fc6a5ee2f95a
RominM <rominmanogil.off@gmail.com>
2026-05-06 18:14:23 +0200

    style: adjusting height to trailer

--- components/global/header/hero-header.vue
+++ components/global/header/hero-header.vue
@@ -190,7 +190,7 @@ watch(
       left: 50%;
       width: 100vw;
       height: 56.25vw;
-      min-height: 100%;
+      min-height: 130%;
       min-width: 177.78vh;
       transform: translate(-50%, -50%);
       border: none;
